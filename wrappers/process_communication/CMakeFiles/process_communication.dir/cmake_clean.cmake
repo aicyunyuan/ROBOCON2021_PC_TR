015011f2d7871cbb3ba91d65722c57c2506028b4
Rujiyuan <62830177+Rujiyuan@users.noreply.github.com>
2021-08-25 21:02:24 +0800

    最新电脑段TR程序

---
+++ wrappers/process_communication/CMakeFiles/process_communication.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process_communication.dir/process_comm.c.o"
+  "libprocess_communication.pdb"
+  "libprocess_communication.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/process_communication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
